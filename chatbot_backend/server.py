62e8800c90e5e38529678121c2f8421e2462d541
black1172 <black.1172@buckeyemail.osu.edu>
2025-08-18 10:21:44 -0400

    trying new link for debug

--- chatbot_backend/server.py
+++ chatbot_backend/server.py
@@ -8,7 +8,7 @@ app = FastAPI()
 # Allow requests from your GitHub Pages domain
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["black1172.github.io/drackett_floor3.github.io/#"],
+    allow_origins=["https://black1172.github.io"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
