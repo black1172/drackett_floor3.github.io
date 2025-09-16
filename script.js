2ebe6eb2d34062ad667063797e8222ea5dffcfd6
Wyatt Black <black.1172@buckeyemail.osu.edu>
2025-09-16 13:24:09 -0400

    localdatetostring updated

--- script.js
+++ script.js
@@ -452,3 +452,11 @@ function setUserCookie() {
     }
 }
 setUserCookie();
+
+function toLocalDateString(dateObj) {
+    // Returns YYYY-MM-DD in local time
+    const yyyy = dateObj.getFullYear();
+    const mm = String(dateObj.getMonth() + 1).padStart(2, '0');
+    const dd = String(dateObj.getDate()).padStart(2, '0');
+    return `${yyyy}-${mm}-${dd}`;
+}
